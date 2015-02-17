9991629795a820bad79fa4938dc16d65f149d41c
biman <biman18@gmail.com>
2015-02-17 16:36:48 -0500

    changes to ibm1

--- ibm1.py
+++ ibm1.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import optparse
 import sys
+import time
 from collections import defaultdict
 
+start = time.time()
 optparser = optparse.OptionParser()
 optparser.add_option("-d", "--data", dest="train", default="data/hansards", help="Data filename prefix (default=data)")
 optparser.add_option("-e", "--english", dest="english", default="e", help="Suffix of English filename (default=e)")
@@ -14,7 +16,7 @@ optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxi
 f_data = "%s.%s" % (opts.train, opts.french)
 e_data = "%s.%s" % (opts.train, opts.english)
 
-sys.stderr.write("Training with Dice's coefficient...")
+#sys.stderr.write("Training with Dice's coefficient...")
 bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:opts.num_sents]]
 e_file = [sentence.strip().split() for sentence in open(e_data).readlines()][:opts.num_sents]
 f_file = [sentence.strip().split() for sentence in open(f_data).readlines()][:opts.num_sents]
@@ -35,9 +37,9 @@ for f in f_vocab:
     for e in e_vocab:   ## might only be required for (e,f) pairs that are in sentence pairs
         t_ef[(e,f)] = 1.0/e_sz
 i=0
-while(i<10):    ##NOT CONVERGED
+while(i<20):    ##NOT CONVERGED
     #print t_ef
-    sys.stderr.write(str(t_ef))
+    #sys.stderr.write(str(t_ef))
     flag=0
     count_ef=defaultdict(float)   #automatically initialized to 0
     f_count = defaultdict(float)  #automatically initialized to 0
@@ -50,7 +52,9 @@ while(i<10):    ##NOT CONVERGED
           for f_i in set(f):
               count_ef[(e_j,f_i)] += t_ef[(e_j,f_i)]/e_count[e_j]
               f_count[f_i] +=  t_ef[(e_j,f_i)]/e_count[e_j]
-    for (e,f) in count_ef.keys():
+    #for (e,f) in count_ef.keys():
+    for f in f_vocab:
+        for e in e_vocab:
             if(abs(count_ef[(e,f)]/f_count[f]-t_ef[(e,f)])>0.01):  #if even one pair has big change, then iterate again
                 flag=1
             t_ef[(e,f)] = count_ef[(e,f)]/f_count[f]
@@ -76,7 +80,12 @@ while(i<10):    ##NOT CONVERGED
 '''
 for (f, e) in bitext:
   for (i, f_i) in enumerate(f):
+    best_prob = 0
+    best_j=0
     for (j, e_j) in enumerate(e):
-      if t_ef[(e_j,f_i)] >= 9.0/e_sz:
-        sys.stdout.write("%i-%i " % (i,j))
+      if t_ef[(e_j,f_i)] >= best_prob:
+        best_prob = t_ef[(e_j,f_i)]
+        best_j  = j
+    sys.stdout.write("%i-%i " % (i,best_j))
   sys.stdout.write("\n")
+sys.stderr.write(str(time.time()-start))
